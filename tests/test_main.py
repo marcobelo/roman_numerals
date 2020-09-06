ccea4fef458c54cfef93f6b2d096ffa992a9747a
marcobelo <marco.barone.belo@gmail.com>
2020-09-06 18:08:45 -0300

    test: converting_IX_X_XI_XII_XIII_to_9_10_11_12_13

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ import pytest
 @pytest.mark.parametrize(
     "roman_number, decimal_number", [("I", 1), ("II", 2), ("III", 3)]
 )
-def test_main_converting_I_II_III_to_1_2_3(roman_number, decimal_number):
+def test_converting_I_II_III_to_1_2_3(roman_number, decimal_number):
     result = main(roman_number)
 
     assert result == decimal_number
@@ -16,7 +16,17 @@ def test_main_converting_I_II_III_to_1_2_3(roman_number, decimal_number):
     "roman_number, decimal_number",
     [("IV", 4), ("V", 5), ("VI", 6), ("VII", 7), ("VIII", 8)],
 )
-def test_main_converting_IV_V_VI_VII_VIII_to_4_5_6_7_8(roman_number, decimal_number):
+def test_converting_IV_V_VI_VII_VIII_to_4_5_6_7_8(roman_number, decimal_number):
+    result = main(roman_number)
+
+    assert result == decimal_number
+
+
+@pytest.mark.parametrize(
+    "roman_number, decimal_number",
+    [("IX", 9), ("X", 10), ("XI", 11), ("XII", 12), ("XIII", 13)],
+)
+def test_converting_IX_X_XI_XII_XIII_to_9_10_11_12_13(roman_number, decimal_number):
     result = main(roman_number)
 
     assert result == decimal_number
