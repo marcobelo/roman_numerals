2339a0acfac8b66f334a914198d51f30900203c6
marcobelo <marco.barone.belo@gmail.com>
2020-09-06 17:58:41 -0300

    test: converting IV, V, VI, VII, VIII to 4, 5, 6, 7, 8

--- tests/test_main.py
+++ tests/test_main.py
@@ -10,3 +10,13 @@ def test_main_converting_I_II_III_to_1_2_3(roman_number, decimal_number):
     result = main(roman_number)
 
     assert result == decimal_number
+
+
+@pytest.mark.parametrize(
+    "roman_number, decimal_number",
+    [("IV", 4), ("V", 5), ("VI", 6), ("VII", 7), ("VIII", 8)],
+)
+def test_main_converting_IV_V_VI_VII_VIII_to_4_5_6_7_8(roman_number, decimal_number):
+    result = main(roman_number)
+
+    assert result == decimal_number
